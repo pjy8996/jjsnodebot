f5f42b5c14c48209fad24ebd88263a743b9a0be9
pjy8996 <pjy8996@naver.com>
2020-08-25 16:54:08 +0900

    add 시바바바ㅏ바바

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 const token = process.env.token;
 const welcomeChannelName = "🔰디스코드🔰";
 const byeChannelName = "🔰디스코드🔰";
-const welcomeChannelComment = "**님! 안녕하세요!\n**사무트서버 디스코드에 오신것을 환영합니다!**\n**원할한 서버이용을 위해서**\n**인증채널에서 인증하신후 이용해주세요!**";
+const welcomeChannelComment = "**님! 안녕하세요!\n**과학서버 디스코드에 오신것을 환영합니다!**\n**원할한 서버이용을 위해서**\n**인증채널에서 인증하신후 이용해주세요!**";
 const byeChannelComment = "**님 안녕히 가세요.. 다음에 또뵈요!**";
 //===================입,퇴장 메세지 끝===================
 
