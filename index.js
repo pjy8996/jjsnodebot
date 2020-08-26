000c7d83a6c9ab6eed9c6c8136298a539c57e663
pjy8996 <pjy8996@naver.com>
2020-08-26 13:35:41 +0900

    add 청소35123

--- index.js
+++ index.js
@@ -103,50 +103,140 @@ client.on('message', (message) => {
       .setFooter(`콜라곰 BOT ❤️`)
       .setTimestamp()
     
-    commandList.forEach(x => {
-      commandStr += `• \`\`${changeCommandStringLength(`${x.name}`)}\`\` : **${x.desc}**\n`;
-    });
-
-    embed.addField('Commands: ', commandStr);
-
-    message.channel.send(embed)
-  }
-
-  if(message.content.startsWith('!전체공지')) {
-    if(checkPermission(message)) return
-    if(message.member != null) { // 채널에서 공지 쓸 때
-      let contents = message.content.slice('!전체공지'.length);
-      message.member.guild.members.array().forEach(x => {
-        if(x.user.bot) return;
-        x.user.send(`<@${message.author.id}> ${contents}`);
+      commandList.forEach(x => {
+        commandStr += `• \`\`${changeCommandStringLength(`${x.name}`)}\`\` : **${x.desc}**\n`;
       });
   
-      return message.reply('공지를 전송했습니다.');
+      embed.addField('Commands: ', commandStr);
+  
+      message.channel.send(embed)
+    } else if(message.content == '!초대코드2') {
+      client.guilds.array().forEach(x => {
+        x.channels.find(x => x.type == 'text').createInvite({maxAge: 0}) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
+          .then(invite => {
+            message.channel.send(invite.url)
+          })
+          .catch((err) => {
+            if(err.code == 50013) {
+              message.channel.send('**'+x.channels.find(x => x.type == 'text').guild.name+'** 채널 권한이 없어 초대코드 발행 실패')
+            }
+          })
+      });
+    } else if(message.content == '!초대코드') {
+      if(message.channel.type == 'dm') {
+        return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
+      }
+      message.guild.channels.get(message.channel.id).createInvite({maxAge: 0}) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
+        .then(invite => {
+          message.channel.send(invite.url)
+        })
+        .catch((err) => {
+          if(err.code == 50013) {
+            message.channel.send('**'+message.guild.channels.get(message.channel.id).guild.name+'** 채널 권한이 없어 초대코드 발행 실패')
+          }
+        })
+    } else if(message.content.startsWith('!전체공지2')) {
+      if(checkPermission(message)) return
+      if(message.member != null) { // 채널에서 공지 쓸 때
+        let contents = message.content.slice('!전체공지2'.length);
+        let embed = new Discord.RichEmbed()
+          .setAuthor('공지 of 콜라곰 BOT')
+          .setColor('#186de6')
+          .setFooter(`콜라곰 BOT ❤️`)
+          .setTimestamp()
+    
+        embed.addField('공지: ', contents);
+    
+        message.member.guild.members.array().forEach(x => {
+          if(x.user.bot) return;
+          x.user.send(embed)
+        });
+    
+        return message.reply('공지를 전송했습니다.');
+      } else {
+        return message.reply('채널에서 실행해주세요.');
+      }
+    } else if(message.content.startsWith('!전체공지')) {
+      if(checkPermission(message)) return
+      if(message.member != null) { // 채널에서 공지 쓸 때
+        let contents = message.content.slice('!전체공지'.length);
+        message.member.guild.members.array().forEach(x => {
+          if(x.user.bot) return;
+          x.user.send(`<@${message.author.id}> ${contents}`);
+        });
+    
+        return message.reply('공지를 전송했습니다.');
+      } else {
+        return message.reply('채널에서 실행해주세요.');
+      }
+    } else if(message.content.startsWith('!청소')) {
+      if(message.channel.type == 'dm') {
+        return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
+      }
+      
+      if(message.channel.type != 'dm' && checkPermission(message)) return
+  
+      var clearLine = message.content.slice('!청소 '.length);
+      var isNum = !isNaN(clearLine)
+  
+      if(isNum && (clearLine <= 0 || 100 < clearLine)) {
+        message.channel.send("**1 ~ 99 까지 메세지청소가 가능합니다!**")
+        return;
+      } else if(!isNum) { // c @나긋해 3
+        if(message.content.split('<@').length == 2) {
+          if(isNaN(message.content.split(' ')[2])) return;
+  
+          var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
+          var count = parseInt(message.content.split(' ')[2])+1;
+          let _cnt = 0;
+  
+          message.channel.fetchMessages().then(collected => {
+            collected.every(msg => {
+              if(msg.author.id == user) {
+                msg.delete();
+                ++_cnt;
+              }
+              return !(_cnt == count);
+            });
+          });
+        }
+      } else {
+        message.channel.bulkDelete(parseInt(clearLine)+1)
+          .then(() => {
+            AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "** 개의 메시지를 삭제했습니다!** - [이 메세지는 5초 후에 사라집니다]");
+          })
+          .catch(console.error)
+      }
+    }
+  });
+  
+  function checkPermission(message) {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+      message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+      return true;
     } else {
-      return message.reply('채널에서 실행해주세요.');
+      return false;
     }
   }
-});
-
-function checkPermission(message) {
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
-    message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
-    return true;
-  } else {
-    return false;
+  
+  function changeCommandStringLength(str, limitLen = 8) {
+    let tmp = str;
+    limitLen -= tmp.length;
+  
+    for(let i=0;i<limitLen;i++) {
+        tmp += ' ';
+    }
+  
+    return tmp;
   }
-}
-
-function changeCommandStringLength(str, limitLen = 8) {
-  let tmp = str;
-  limitLen -= tmp.length;
-
-  for(let i=0;i<limitLen;i++) {
-      tmp += ' ';
+  
+  async function AutoMsgDelete(message, str, delay = 5000) {
+    let msg = await message.channel.send(str);
+  
+    setTimeout(() => {
+      msg.delete();
+    }, delay);
   }
-
-  return tmp;
-}
-
-
-client.login(token);
+  
+  
+  client.login(token);
